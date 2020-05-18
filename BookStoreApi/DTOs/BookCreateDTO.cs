18f75a7eac54a801f349f5c7d86843cce8c6d35a
Sudi Simba <sudisimbadav@gmail.com>
2020-05-18 15:58:56 +0200

    Implemented JWT token and tested it

---
+++ BookStoreApi/DTOs/BookCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApi.DTOs
+{
+    public class BookCreateDTO
+    {
+        [Required]
+        public string Title { get; set; }
+        public int? Year { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [StringLength(500)]
+        public string Summary { get; set; }
+        public string Image { get; set; }
+        public decimal? Price { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+    }
+}
---
+++ BookStoreApi/DTOs/BookUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApi.DTOs
+{
+    public class BookUpdateDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int? Year { get; set; }
+        [StringLength(500)]
+        public string Summary { get; set; }
+        public string Image { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
